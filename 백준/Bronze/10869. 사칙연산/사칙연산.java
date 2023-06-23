ed98bc0a2ad7d6fb24e6a78951157280991b3dfa
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-06-23 14:59:45 +0900

    [Bronze V] Title: 사칙연산, Time: 216 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A,B;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+    }
+
+}
