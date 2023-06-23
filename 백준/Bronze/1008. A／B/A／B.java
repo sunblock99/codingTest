926f45b20bc9ef11617907e0f687c26543ad44e8
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-06-23 14:58:04 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A,B;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+        System.out.println(A / B);
+
+    }
+
+}
