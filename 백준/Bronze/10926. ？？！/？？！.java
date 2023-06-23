63e8a63162baf8d788c7c528ee725cff1fce5269
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-06-23 15:11:44 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+
+        System.out.println(str + "??!");
+
+    }
+
+}
