42b7449b12d2cf6fe26bc19dc2506f751c051aac
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-06-26 10:31:30 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+
+        for(int i = 1; i < 10; i++){
+            System.out.println(A + " * " + i + " = " + A * i);
+        }
+
+    }
+
+}
