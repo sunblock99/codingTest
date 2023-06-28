52317a7db8c4e8b8da51ff296465da90aa6dcdd1
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-06-28 20:15:27 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] agrs){
+        System.out.print("\\    /\\\n");
+        System.out.print(" )  ( \')\n");
+        System.out.print("(  /  )\n");
+        System.out.println(" \\(__)|");
+    }
+}
