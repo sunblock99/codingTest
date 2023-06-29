128f83e8ecdf578662d64dc24b6b85f98cee335c
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-06-29 15:09:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 216 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char ch = str.charAt(0);
+        int num = (int)ch;
+
+        System.out.println(num);
+
+    }
+}
