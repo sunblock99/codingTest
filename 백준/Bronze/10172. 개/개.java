4c19f333fee6f79efc853864921df31cc72c4543
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-07-07 15:39:14 +0900

    [Bronze V] Title: 개, Time: 108 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+
+class Main {
+
+    public static void main(String[] args) {
+      
+       String dog = "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|";
+
+        System.out.println(dog);
+    }
+    }
+
