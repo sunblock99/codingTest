cfc5e2f33c7d03c106e2727c2d64dba51b8e2a0f
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-07-28 16:49:40 +0900

    [Bronze V] Title: N 찍기, Time: 1060 ms, Memory: 32968 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+
+        for(int i = 0; i < A; i++){
+            System.out.println(i + 1);
+        }
+    }
+}
