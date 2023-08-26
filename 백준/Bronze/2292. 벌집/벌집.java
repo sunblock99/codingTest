a5479f0b9a227d58d3244663bebd0ddfc2049f8b
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-08-26 19:49:21 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void  main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 2;
+
+        if(N == 1) {
+            System.out.println(1);
+        }else{
+            while (range <= N){
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
