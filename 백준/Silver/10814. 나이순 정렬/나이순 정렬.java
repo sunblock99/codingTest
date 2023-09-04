de8e49ef263115ecc21b71aa949a7a6891bcd355
선형채 <105029091+sunblock99@users.noreply.github.com>
2023-09-04 15:41:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 1992 ms, Memory: 104952 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int N = sc.nextInt();
+        String[][] arr = new String[N][2];
+
+
+        for(int i = 0; i < N; i++) {
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+            }
+        });
+
+        for(int i = 0; i < N; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+}
